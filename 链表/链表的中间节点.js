42a57ee40dffa2e01f5f36b1c835355b4e556f67
curtainTan <1948750473@qq.com>
2020-03-23 00:29:31 +0800

    每日一题-链表的中间节点

---
+++ 链表/链表的中间节点.js
@@ -0,0 +1,30 @@
+// 876. 链表的中间结点
+// https://leetcode-cn.com/problems/middle-of-the-linked-list/
+
+
+// 数组法
+var middleNode = function(head) {
+    let queue = []
+    let next = head
+    while( next !== null ){
+        queue.push( next )
+        next = next.next
+    }
+    next = queue[ Math.floor( queue.length / 2 ) ]
+    return next
+};
+
+
+// 快慢指针法
+
+var middleNode = function(head) {
+    let slow = head
+    let fast = head.next
+    while( fast !== null ){
+        fast = fast.next && fast.next.next
+        slow = slow.next
+    }
+    return slow
+};
+
+
